2e436aa3cb3f998562a490bff11e3573db1bd77d
Samuel Pereira Brandão <samuelpbrandao58@gmail.com>
2022-01-20 22:52:28 -0300

    alguns exer

---
+++ variaveis-variaveis.php
@@ -0,0 +1,18 @@
+<?php
+$bebida = "refrigerante";
+$$bebida = "Guarana";
+echo "$refrigerante $bebida <br>";
+
+///////////////////////////////
+$destino = "cidade";
+$$destino = "são paulo";
+
+echo $cidade;
+
+$nome = "samuel";
+$$nome = "pereira";
+echo  "$nome,$samuel <br>";
+
+$bebida = "suco";
+$$bebida = "delvale";
+echo "sim hoje iremos tomar um $suco!";
---
+++ variaveis.php
@@ -0,0 +1,38 @@
+<?php 
+//meus dados
+$nome = "Samuel Pereira";
+$idade = 21;
+$altura = 1.80;
+
+echo "Meu nome é $nome, minha idade é de $idade anos, e minha altura é de $altura m! <br>";
+
+// alterando meus dados
+$nome = "Felipe cabral";
+$idade = 75;
+$altura = 1.55;
+
+echo "Meu nome é $nome, minha idade é de $idade anos, e minha altura é de $altura m! <br>";
+echo "<hr>";
+$zarro = "Hilux <br>";
+$Zarro = "outra hilux <br>";
+echo $zarro;
+echo $Zarro;
+$meunome = "Samuel";
+$meuNome = "Gustavo";
+echo "$meunome e $meuNome";
+
+/*nomes validos
+$nome
+$_meunome
+$carro10 
+$nota1
+$meu_nome
+$meunNome
+*/
+
+/* nomes invalidos 
+$13432432
+$carro*^%$#@
+$meu carro
+
+*/
