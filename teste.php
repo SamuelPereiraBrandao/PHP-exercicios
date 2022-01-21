3dc17fb1a933e70a9fc41ee8557455049c632b55
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2022-01-20 22:48:11 -0300

    Add files via upload

---
+++ teste.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Meu nome e samuel </br>"; 
+    print "Minha idade é 21 anos!";
+    //beleza
+    /* aqui podemos comentar avontade
+    tendeu? */
+    ?>
+</body>
+</html>
