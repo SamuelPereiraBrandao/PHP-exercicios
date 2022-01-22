bfeae3e8e8e013339e134daf9a3cc1df687072fe
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2022-01-21 21:19:18 -0300

    Add files via upload

---
+++ aspas-concatenacao.php
@@ -0,0 +1,6 @@
+<?php
+$nome = 'Samuel Pereira';
+echo 'Meu nome é $nome <br>'; //aspas simples são literais! considerado texto
+echo 'Meu nome é '.$nome.' e minha idade é \'21\' anos!';
+echo '<hr> <br>';
+echo "Meu nome é \"$nome\"";
