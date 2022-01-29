38b2463ca408fe11fe63e89d0e50f124de866843
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2022-01-29 16:39:47 -0300

    Add files via upload

---
+++ while e do while.php
@@ -0,0 +1,22 @@
+<?php
+$contador = 1;
+
+while ($contador <= 10): //pode usar o {} tbm
+    echo "contador eh $contador <br>";
+    $contador++;
+    
+endwhile;
+
+
+//////////////
+
+$contador = 1;
+do {
+    echo "novo c é $contador <br>";
+    $contador++;
+
+} while($contador <=10);
+
+
+//while = enquanto
+// do = faça
