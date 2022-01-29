5ce8391c3a94feebef3b4d76da72c5b552154f65
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2022-01-29 16:38:42 -0300

    Add files via upload

---
+++ operadores aritmeticos.php
@@ -0,0 +1,44 @@
+<?php
+echo 10 + 10;
+echo "<br>";
+echo 15 - 5;
+echo "<br>";
+echo 3 * 5;
+echo "<br>";
+echo 15 / 5;
+echo "<br>";
+echo 15 % 4;
+echo "<br>";
+ # 15 ** 4
+$a = 10;
+$b = 20;
+$c = 30;
+$d = 5;
+$e = 16;
+
+echo "<hr>";
+$adicao =  $a + $b;
+echo $adicao;
+echo "<hr>";
+
+$subtracao = $c - $a;
+echo $subtracao;
+echo "<hr>";
+
+$multiplicacao = $d * $a;
+echo $multiplicacao;
+echo "<hr>";
+
+$divisao = $c / $a;
+echo $divisao;
+echo "<hr>";
+
+$modulo = $e % $d;
+echo $modulo;
+echo "<hr>";
+
+$expo = $a ** $d;
+echo $expo;
+echo "<hr>";
+
+echo (5 + 7 + 9 + 8) / 4;
