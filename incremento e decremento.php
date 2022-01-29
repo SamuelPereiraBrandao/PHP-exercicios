9432d07d174c9231b72029220ec7e550c5385227
Samuel Pereira Brandão <89025317+SamuelPereiraBrandao@users.noreply.github.com>
2022-01-29 16:38:52 -0300

    Add files via upload

---
+++ incremento e decremento.php
@@ -0,0 +1,22 @@
+<?php
+$valor = 20;
+//pre-incremento
+echo ++$valor;
+
+//pos-incremento
+echo $valor++;
+echo "<br>";
+echo $valor;
+echo "<br>";
+
+//pre decremento
+echo --$valor;
+echo "<br>";
+
+//pos-decremento
+echo $valor--;
+echo "<br>";
+echo $valor;
+echo "<br>";
+
+//
